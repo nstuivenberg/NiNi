9ca1b85fc7300b48e1a496106280059c5e510641
Nick Stuivenberg <nstuivenberg@outlook.com>
2019-03-09 12:37:38 +0100

    Structure, classes, javadoc

---
+++ src/main/java/nl/hu/bdsd/Spider.java
@@ -0,0 +1,16 @@
+package nl.hu.bdsd;
+
+/**
+ * This simulates a spider that crawls the web.
+ *
+ * The spider program represents the spider that crawls the web looking for news-articles.
+ * This spides reads it files from a JSON-file and publishes them one by one onto the messagebus.
+ *
+ * @since   2019-03-08
+ */
+
+public class Spider {
+    public static void main(String[] args) {
+
+    }
+}
---
+++ src/main/java/nl/hu/bdsd/spider/JSONReader.java
@@ -0,0 +1,10 @@
+package nl.hu.bdsd.spider;
+
+/**
+ *
+ *
+ * @since   2019-03-08
+ */
+public class JSONReader {
+
+}
